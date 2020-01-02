2506bec2c73f323ae1f30a2f7b525ef8fdc95712
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2020-01-01 22:58:24 -0800

    Added GA

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/scss/main.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: {
+    id: 'G-MNTXDS0Z7G',
+    'anonymize_ip': true
+  }
+});
+
 
 new Vue({
   router,
