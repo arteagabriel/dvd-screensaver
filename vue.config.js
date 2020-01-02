4f8a451aa6ee34773656f4faa5875dcb61b246de
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2020-01-01 22:40:52 -0800

    Fixed css modules config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   css: {
+    requireModuleExtension: false,
     loaderOptions: {
       css: {
-        localIdentName: process.env.NODE_ENV === 'production' ? 'dvd-[hash:base64:8]' : '[local]_[hash:base64:8]'
+        modules: {
+          localIdentName: process.env.NODE_ENV === 'production' ? 'dvd-[hash:base64:8]' : '[name]_[local]_[hash:base64:8]'
+        },
       },
       sass: {
         prependData: `@import "@/assets/scss/global.scss";`
