cb27f8f83e65073efedb7059109d922e4229f689
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2020-01-01 22:34:02 -0800

    Updated production css names

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   css: {
     loaderOptions: {
+      css: {
+        localIdentName: process.env.NODE_ENV === 'production' ? 'dvd-[hash:base64:8]' : '[local]_[hash:base64:8]'
+      },
       sass: {
         prependData: `@import "@/assets/scss/global.scss";`
       }
